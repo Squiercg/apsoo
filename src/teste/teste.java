6c3b0546539d7c8922e501c6d769e7c81adb1b59
Nazario <Bruno@Skurai>
2015-05-19 18:34:58 -0400

    Testando Commit.

--- src/teste/teste.java
+++ src/teste/teste.java
@@ -3,9 +3,7 @@ package teste;
 public class teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(1+1);
-		System.out.println("Teste");
+		System.out.println("Hello");
 	}
 
 }

f95d43e32df89dd041256ce6b5593d5897e46239
Nazario <Bruno@Skurai>
2015-05-19 21:16:37 -0400

    Atualizando bases, criando classe main para testes de interface gráfica.

--- src/interfaces/SystemInterface.java
+++ src/interfaces/SystemInterface.java
@@ -1,6 +1,12 @@
 package interfaces;
 
+import java.awt.BorderLayout;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -38,6 +44,47 @@ public class SystemInterface {
 	private Border systemInterfaceBorderEmpty;
 	
 	public SystemInterface() {
+		setBorders();
+		setSystemInterfaceFrame();
 		
+		setSystemInterface();
+	}
+	
+	private void setBorders() {
+		systemInterfaceBorderDefault = BorderFactory.createRaisedBevelBorder();
+		systemInterfaceBorderEmpty = BorderFactory.createEmptyBorder();
+	}
+	
+	private void setSystemInterfaceFrame() {
+		systemInterfaceFrame = new JFrame("SCVE-CdT");
+		try {
+			String systemIconImagePath = new File("../lib/.").getCanonicalPath() + "\\" + "CDT_icon.PNG";
+			systemInterfaceFrame.setIconImage((new ImageIcon(systemIconImagePath)).getImage());
+		}
+		catch (IOException exPathNotFound) {
+			systemInterfaceLabelStatus.setText("A imagem do icone do sistema nao foi encontrada!");
+		}
+		systemInterfaceFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		systemInterfaceFrame.setLayout(new BorderLayout());
+		systemInterfaceFrame.setSize(1024, 768);
+	}
+	
+	private void setSystemInterface() {
+		try {
+			String systemWelcomeImagePath = new File("../lib/.").getCanonicalPath() + "\\" + "CDT_welcome.jpg";
+			JLabel systemWelcomeImage = new JLabel(new ImageIcon(systemWelcomeImagePath));
+			systemInterfaceFrame.add(systemWelcomeImage);
+			systemInterfaceFrame.setVisible(true);
+		}
+		catch(IOException exPathNotFound) {
+			systemInterfaceLabelStatus.setText("Imagem da tela de login nao encontrada!");
+		}
+		try {
+		    Thread.sleep(1337);
+		}
+		catch(InterruptedException exThreadFailed) {
+			systemInterfaceLabelStatus.setText("Houve algum erro ao carregar o programa!");
+		    Thread.currentThread().interrupt();
+		}
 	}
 }

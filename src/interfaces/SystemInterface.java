60ce8112560730bdf5462419879610cacde6c428
Nazario <Bruno@Skurai>
2015-05-20 02:34:35 -0400

    Inclusas diversas funcionalidades da interface gráfica principal.

--- src/interfaces/SystemInterface.java
+++ src/interfaces/SystemInterface.java
@@ -6,8 +6,10 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -48,15 +50,17 @@ public class SystemInterface {
 	private String systemInterfaceStatusMessage;
 	
 	public SystemInterface() {
-		setBorders();
+		setSystemDefaultBorders();
 		setSystemInterfaceFrame();
 		setSystemInterfaceLabelStatus();
 		setSystemInterfaceMenuBar();
-		setHomeButton();
+		setSystemHomeButton();
+		setSystemInterfaceMenu();
+		setSystemExitButton();
 		setSystemInterface();
 	}
 	
-	private void setBorders() {
+	private void setSystemDefaultBorders() {
 		systemInterfaceBorderDefault = BorderFactory.createRaisedBevelBorder();
 		systemInterfaceBorderEmpty = BorderFactory.createEmptyBorder();
 	}
@@ -71,7 +75,7 @@ public class SystemInterface {
 			systemInterfaceLabelStatus.setText("A imagem do icone do sistema nao foi encontrada!");
 			System.out.print(systemInterfaceLabelStatus.getText());
 		}
-		systemInterfaceFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		systemInterfaceFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		systemInterfaceFrame.setLayout(new BorderLayout());
 		systemInterfaceFrame.setSize(1024, 768);
 	}
@@ -90,19 +94,54 @@ public class SystemInterface {
 		systemInterfaceFrame.setJMenuBar(systemInterfaceMenuBar);
 	}
 	
-	private void setHomeButton() {
-		JLabel systemInterfaceLabelHome = new JLabel("   Home  ");
-		systemInterfaceLabelHome.setFont(new Font(null, Font.PLAIN + Font.BOLD, systemInterfaceLabelStatus.getFont().getSize() + 2));
+	private void setSystemHomeButton() {
+		JLabel systemInterfaceLabelHome = new JLabel("   Home   ");
+		systemInterfaceLabelHome.setFont(new Font(null, Font.PLAIN + Font.BOLD, systemInterfaceLabelHome.getFont().getSize() + 4));
+		systemInterfaceLabelHome.setVerticalAlignment(JLabel.BOTTOM);
 		systemInterfaceLabelHome.addMouseListener(new HandlerHomeButton());
 		systemInterfaceMenuBar.add(systemInterfaceLabelHome);
 	}
 	
+	private void setSystemInterfaceMenu() {
+		ArrayList<String> systemInterfaceMenuNames = new ArrayList<String>();
+		systemInterfaceMenuNames.add("Lotes");
+		systemInterfaceMenuNames.add("Vendas");
+		systemInterfaceMenuNames.add("Clientes");
+		systemInterfaceMenuNames.add("Fornecedores");
+		systemInterfaceMenuNames.add("Categorias");
+		systemInterfaceMenuNames.add("Produtos");
+		ArrayList<String> systemInterfaceMenuItemNames = new ArrayList<String>();
+		systemInterfaceMenuItemNames.add("Cadastrar");
+		systemInterfaceMenuItemNames.add("Consultar");
+		for(String systemInterfaceMenuName : systemInterfaceMenuNames) {
+			systemInterfaceMenu = new JMenu(systemInterfaceMenuName);
+			systemInterfaceMenu.setFont(new Font(null, Font.PLAIN + Font.BOLD, systemInterfaceMenu.getFont().getSize() + 4));
+			systemInterfaceMenu.addMouseListener(new HandlerMenuOptions(systemInterfaceMenuName, null));
+			for(String systemInterfaceMenuItemName : systemInterfaceMenuItemNames) {
+				systemInterfaceMenuItem = new JMenuItem(systemInterfaceMenuItemName);
+				systemInterfaceMenuItem.setFont(new Font(null, Font.PLAIN + Font.BOLD, systemInterfaceMenuItem.getFont().getSize() + 2));
+				systemInterfaceMenuItem.addMouseListener(new HandlerMenuOptions(systemInterfaceMenuName, systemInterfaceMenuItemName));
+				systemInterfaceMenu.add(systemInterfaceMenuItem);
+			}
+			systemInterfaceMenuBar.add(systemInterfaceMenu);
+		}
+	}
+	
+	private void setSystemExitButton() {
+		JLabel systemInterfaceLabelExit = new JLabel("   Sair   ");
+		systemInterfaceLabelExit.addMouseListener(new HandlerExitButton());
+		systemInterfaceLabelExit.setFont(new Font(null, Font.PLAIN + Font.BOLD, systemInterfaceLabelExit.getFont().getSize() + 4));
+		systemInterfaceLabelExit.setVerticalAlignment(JLabel.BOTTOM);
+		systemInterfaceMenuBar.add(Box.createHorizontalGlue());
+		systemInterfaceMenuBar.add(systemInterfaceLabelExit);
+	}
+	
 	private void setSystemInterface() {
-		JLabel systemWelcomeImage = null;
+		systemInterfaceLabelImage = null;
 		try {
 			String systemWelcomeImagePath = new File("lib/.").getCanonicalPath() + "\\" + "CDT_welcome.jpg";
-			systemWelcomeImage = new JLabel(new ImageIcon(systemWelcomeImagePath));
-			systemInterfaceFrame.add(systemWelcomeImage);
+			systemInterfaceLabelImage = new JLabel(new ImageIcon(systemWelcomeImagePath));
+			systemInterfaceFrame.add(systemInterfaceLabelImage);
 			systemInterfaceFrame.setVisible(true);
 			try {
 			    Thread.sleep(1337 * 2);
@@ -111,7 +150,7 @@ public class SystemInterface {
 				systemInterfaceLabelStatus.setText("Houve algum erro ao carregar o programa!");
 			    Thread.currentThread().interrupt();
 			}
-			systemInterfaceFrame.remove(systemWelcomeImage);
+			systemInterfaceFrame.remove(systemInterfaceLabelImage);
 		}
 		catch(IOException exPathNotFound) {
 			systemInterfaceLabelStatus.setText("Imagem da tela de login nao encontrada!");
@@ -158,6 +197,102 @@ public class SystemInterface {
 		@Override
 		public void mouseEntered(MouseEvent e) {
 			systemInterfaceLabelStatus.setText("Retorna à tela principal");
+		}
+		
+		@Override
+		public void mouseExited(MouseEvent e) {
+			systemInterfaceLabelStatus.setText(systemInterfaceStatusMessage);
+		}
+		
+		@Override
+		public void mousePressed(MouseEvent e) {
+			mouseEntered(e);
+		}
+		
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			mouseClicked(e);
+		}
+	}
+	
+	private class HandlerExitButton implements MouseListener {
+		
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			try {
+			    Thread.sleep(1337 * 2);
+			    systemInterfaceFrame.dispose();
+			}
+			catch(InterruptedException exThreadFailed) {
+				systemInterfaceLabelStatus.setText("Houve algum erro ao encerrar o programa!");
+			    Thread.currentThread().interrupt();
+			}
+		}
+		
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			systemInterfaceLabelStatus.setText("Encerra o sistema");
+		}
+		
+		@Override
+		public void mouseExited(MouseEvent e) {
+			systemInterfaceLabelStatus.setText(systemInterfaceStatusMessage);
+		}
+		
+		@Override
+		public void mousePressed(MouseEvent e) {
+			clearSystemInterface();
+			systemInterfaceLabelStatus.setText("Encerrando conexoes..");
+		}
+		
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			mouseClicked(e);
+		}
+	}
+	
+	private class HandlerMenuOptions implements MouseListener {
+		
+		private String systemInterfaceMenuName;
+		private String systemInterfaceMenuItemName;
+		
+		public HandlerMenuOptions(String systemInterfaceMenuName, String systemInterfaceMenuItemName) {
+			this.systemInterfaceMenuName = systemInterfaceMenuName;
+			this.systemInterfaceMenuItemName = systemInterfaceMenuItemName;
+		}
+		
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if(!(e.getSource() instanceof JMenu)) {
+				systemInterfaceStatusMessage = 
+					String.format("Home > %s > %s %s", systemInterfaceMenuName, systemInterfaceMenuItemName, systemInterfaceMenuName.toLowerCase());
+				systemInterfaceLabelStatus.setText(systemInterfaceStatusMessage);
+				if(e.getSource() instanceof JMenuItem) {
+					clearSystemInterface();
+					if(systemInterfaceMenuItemName.equals("Cadastrar")) {
+//						cadastraAlgo(systemInterfaceMenuName); // Implementar
+					}
+					else if(systemInterfaceMenuItemName.equals("Consultar")) {
+//						consultaAlgo(systemInterfaceMenuName); // Implementar
+					}
+				}
+			}
+			else {
+				mouseEntered(e);
+			}
+		}
+		
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			systemInterfaceStatusMessage = systemInterfaceStatusMessage != null && e.getSource() instanceof JMenu &&  
+				!(systemInterfaceStatusMessage.equals(systemInterfaceLabelStatus.getText())) ?  
+					systemInterfaceStatusMessage : systemInterfaceLabelStatus.getText();
+			if(e.getSource() instanceof JMenu) {
+				systemInterfaceLabelStatus.setText("Acessa o modulo de " + systemInterfaceMenuName.toLowerCase());
+			}
+			else {
+				systemInterfaceLabelStatus.setText(systemInterfaceMenuItemName + " " + systemInterfaceMenuName.toLowerCase()); 
+			}
 		}
 		
 		@Override

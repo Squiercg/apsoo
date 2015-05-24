56cf0e54287e4ab323bd27336eacfbf714bffbad
Doglas <Doglas@Doglas-dell>
2015-05-24 18:23:33 -0400

    desfiz cagada do genericDao

--- src/repositorio/GenericDao.java
+++ src/repositorio/GenericDao.java
@@ -1,22 +1,19 @@
 package repositorio;
 
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
-import com.j256.ormlite.support.ConnectionSource;
 
 public abstract class GenericDao<E> {
 
 	protected Dao<E, Integer> dao;
 	private Class<E> type;
-	private ConnectionSource connectionSource;
+	private JdbcConnectionSource connectionSource;
  
 	public GenericDao(String databaseUrl, Class<E> type) throws SQLException {
-		DriverManager.registerDriver(new org.sqlite.JDBC());
 		this.type = type;
 		connectionSource = new JdbcConnectionSource(databaseUrl);
 		setDao();

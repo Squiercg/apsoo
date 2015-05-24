d72d37b93374ea46908fb4a3b63728b5aa054dd8
Nazario <Bruno@Skurai>
2015-05-23 21:05:11 -0400

    Grandes avanços na parte gráfica.

--- src/interfaces/Methods.java
+++ src/interfaces/Methods.java
@@ -1,44 +1,114 @@
 package interfaces;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 public class Methods {
 	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static JPanel cadastraLote(Dimension preferredSize) {
-		JPanel mainPanel = new JPanel(new BorderLayout());
-		Border defaultBorder = BorderFactory.createEmptyBorder();
-		makeLateralBorders(mainPanel, preferredSize, defaultBorder);
+		JPanel panelLevel0 = new JPanel(new BorderLayout());
+		Boolean isBrunoTesting = false;
+		Border defaultBorder = isBrunoTesting ? BorderFactory.createRaisedBevelBorder() : BorderFactory.createEmptyBorder();
+		makeLateralBorders(panelLevel0, preferredSize, defaultBorder);
 		
-		JPanel sectionPanel = new JPanel(new BorderLayout());
-		mainPanel.add(sectionPanel, BorderLayout.CENTER);
+		JPanel panelLevel1 = new JPanel(new BorderLayout());
+		panelLevel0.add(panelLevel1, BorderLayout.CENTER);
 		
-		JPanel workingPanel = new JPanel(new BorderLayout());
-		sectionPanel.add(workingPanel, BorderLayout.NORTH);
+		JPanel panelLevel2 = new JPanel(new BorderLayout());
+		panelLevel1.add(panelLevel2, BorderLayout.NORTH);
 		
 		JLabel placeHolder = new JLabel("");
 		placeHolder.setPreferredSize(new Dimension((int) (preferredSize.getWidth()), (int) (preferredSize.getHeight() / 32)));
-		workingPanel.add(placeHolder, BorderLayout.NORTH);
+		placeHolder.setBorder(defaultBorder);
+		panelLevel2.add(placeHolder, BorderLayout.NORTH);
 		
 		placeHolder = new JLabel("Cadastro de Lotes");
 		placeHolder.setBorder(defaultBorder);
 		placeHolder.setFont(new Font(null, Font.PLAIN + Font.BOLD, placeHolder.getFont().getSize() + 20));
-		workingPanel.add(placeHolder, BorderLayout.CENTER);
+		panelLevel2.add(placeHolder, BorderLayout.CENTER);
 		
-		String[] petStrings = { "Bird", "Cat", "Dog", "Rabbit", "Pig" };
-		JComboBox petList = new JComboBox(petStrings);
-		petList.setSelectedIndex(4);
-		workingPanel.add(petList, BorderLayout.SOUTH);
+		placeHolder = new JLabel("");
+		placeHolder.setPreferredSize(new Dimension((int) (preferredSize.getWidth()), (int) (preferredSize.getHeight() / 16)));
+		placeHolder.setBorder(defaultBorder);
+		panelLevel2.add(placeHolder, BorderLayout.SOUTH);
 		
+		panelLevel2 = new JPanel(new BorderLayout());
+		panelLevel1.add(panelLevel2, BorderLayout.CENTER);
 		
-		return mainPanel;
+		JPanel panelLevel3 = new JPanel(new BorderLayout());
+		panelLevel2.add(panelLevel3, BorderLayout.NORTH);
+		
+		placeHolder = new JLabel("Fornecedor");
+		placeHolder.setBorder(defaultBorder);
+		placeHolder.setFont(new Font(null, Font.PLAIN + Font.BOLD, placeHolder.getFont().getSize() + 7));
+		placeHolder.setPreferredSize(new Dimension((int) (preferredSize.getHeight() / 64 + (int) (preferredSize.getWidth() / 3)), 
+			(int) (preferredSize.getHeight() / 32)));
+		panelLevel3.add(placeHolder, BorderLayout.WEST);
+		
+		placeHolder = new JLabel("Data da inclusão");
+		placeHolder.setBorder(defaultBorder);
+		placeHolder.setFont(new Font(null, Font.PLAIN + Font.BOLD, placeHolder.getFont().getSize() + 7));
+		panelLevel3.add(placeHolder, BorderLayout.CENTER);
+		
+		panelLevel3 = new JPanel(new BorderLayout());
+		panelLevel2.add(panelLevel3, BorderLayout.CENTER);
+		JPanel panelLevel4 = new JPanel(new BorderLayout());
+		panelLevel3.add(panelLevel4, BorderLayout.NORTH);
+		
+		String[] listaClientes = {"Augusto Ribas", "Bruno Nazário", "Doglas Sorgatto", "Thiago Machado", "André Fulano"};
+		JComboBox comboBox = new JComboBox(listaClientes);
+		comboBox.setPreferredSize(new Dimension((int) (preferredSize.getWidth() / 4), (int) (preferredSize.getHeight() / 32)));
+		comboBox.setBackground(Color.white);
+		comboBox.setEditable(false);
+		comboBox.setSelectedIndex(1);
+		panelLevel4.add(comboBox, BorderLayout.WEST);
+		
+		JPanel panelLevel5 = new JPanel(new BorderLayout());
+		panelLevel4.add(panelLevel5, BorderLayout.CENTER);
+		
+		placeHolder = new JLabel("");
+		placeHolder.setBorder(defaultBorder);
+		placeHolder.setPreferredSize(new Dimension((int) (preferredSize.getWidth() / 3) + (int) (preferredSize.getWidth() / 64) - 
+			(int) (preferredSize.getWidth() / 4), (int) (preferredSize.getHeight() / 32)));
+		panelLevel5.add(placeHolder, BorderLayout.WEST);
+		
+		JTextField textField = new JTextField();
+		textField.setText((new SimpleDateFormat("dd/MM/yyyy")).format(new Date()));
+		float[] hsbColor = Color.RGBtoHSB(184, 207, 229, null);
+		Color innerBorderColor = Color.getHSBColor(hsbColor[0], hsbColor[1], hsbColor[2]);
+		hsbColor = Color.RGBtoHSB(122, 138, 153, null);
+		Color outerBorderColor = Color.getHSBColor(hsbColor[0], hsbColor[1], hsbColor[2]);
+		textField.setBorder(BorderFactory.createCompoundBorder(BorderFactory
+			.createLineBorder(outerBorderColor), BorderFactory.createLineBorder(innerBorderColor)));
+		textField.setPreferredSize(new Dimension((int) (preferredSize.getWidth() / 3), (int) (preferredSize.getHeight() / 32)));
+		panelLevel5.add(textField, BorderLayout.CENTER);
+		
+		placeHolder = new JLabel("");
+		placeHolder.setBorder(defaultBorder);
+		placeHolder.setPreferredSize(new Dimension((int) (preferredSize.getWidth() / 4), (int) (preferredSize.getHeight() / 32)));
+		panelLevel5.add(placeHolder, BorderLayout.EAST);
+		
+		panelLevel3 = new JPanel(new BorderLayout());
+		panelLevel2.add(panelLevel3, BorderLayout.SOUTH);
+		
+		placeHolder = new JLabel("");
+		placeHolder.setBorder(defaultBorder);
+		placeHolder.setPreferredSize(new Dimension((int) (preferredSize.getWidth() / 64), (int) (preferredSize.getHeight() / 32)));
+		panelLevel3.add(placeHolder, BorderLayout.CENTER);
+		
+		return panelLevel0;
 	}
 	
 	private static void makeLateralBorders(JPanel panel, Dimension reference, Border style) {

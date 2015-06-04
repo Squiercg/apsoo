7267472bed277e3c543d5f27b2c8a0c4b34754ad
Doglas <Doglas@Doglas-dell>
2015-06-04 19:56:30 -0400

    Mudanca nos atributos de estoque (padrao Java)

--- src/classes/Estoque.java
+++ src/classes/Estoque.java
@@ -7,46 +7,46 @@ import com.j256.ormlite.table.DatabaseTable;
 public class Estoque {
 	
 	@DatabaseField(generatedId = true, columnName = "estoque_id")
-	private int estoque_id;
+	private int estoqueId;
 	@DatabaseField(canBeNull = false, columnName = "estoque_produto_id")
-	private int estoque_produto_id;
+	private int estoqueProdutoId;
 	@DatabaseField(canBeNull = false, columnName = "estoque_produto_quantidade")
-	private int estoque_produto_quantidade;
+	private int estoqueProdutoQtde;
 		
 	public Estoque() {}
 	
 	public Estoque(
-			int estoque_id, 
-			int estoque_produto_id,
-			int estoque_produto_quantidade) {
+			int estoqueId, 
+			int estoqueProdutoId,
+			int estoqueProdutoQtde) {
 
-		this.estoque_id = estoque_id;
-		this.estoque_produto_id = estoque_produto_id;
-		this.estoque_produto_quantidade = estoque_produto_quantidade;
+		this.estoqueId = estoqueId;
+		this.estoqueProdutoId = estoqueProdutoId;
+		this.estoqueProdutoQtde = estoqueProdutoQtde;
 	}
 	
 	public int getEstoque_id() {
-		return estoque_id;
+		return estoqueId;
 	}
 	
-	public void setEstoque_id(int estoque_id) {
-		this.estoque_id = estoque_id;
+	public void setEstoque_id(int estoqueId) {
+		this.estoqueId = estoqueId;
 	}
 	
 	public int getEstoque_produto_id() {
-		return estoque_produto_id;
+		return estoqueProdutoId;
 	}
 	
-	public void setEstoque_produto_id(int estoque_produto_id) {
-		this.estoque_produto_id = estoque_produto_id;
+	public void setEstoque_produto_id(int estoqueProdutoId) {
+		this.estoqueProdutoId = estoqueProdutoId;
 	}
 	
 	public int getEstoque_produto_quantidade() {
-		return estoque_produto_quantidade;
+		return estoqueProdutoQtde;
 	}
 	
-	public void setEstoque_produto_quantidade(int estoque_produto_quantidade) {
-		this.estoque_produto_quantidade = estoque_produto_quantidade;
+	public void setEstoque_produto_quantidade(int estoqueProdutoQtde) {
+		this.estoqueProdutoQtde = estoqueProdutoQtde;
 	}
 	
 }
